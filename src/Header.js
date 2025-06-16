efafe069b649481076869c5a3bfe59547799b1a1
Mitul Dial <mituldial.lh116@gmail.com>
2025-06-16 13:41:59 +0500

    Added meta tags for SEO and social sharing

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header>
+            <img src="M:\coursera\web development\Final project\icons_assets\Mario and Adrian A.jpg" alt="Little lemon logo" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+    return (
+        <main>
+            { }///this will contain the content of the website
+        </main>
+    );
+}
+
+export default Main;
